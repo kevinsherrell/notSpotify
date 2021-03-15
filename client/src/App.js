be476cb239a190fe717400ba53caa3ab42ad81b8
Jacob Greenberg <jacobgreenberg@Jacobs-MacBook-Pro.local>
2021-03-15 19:04:55 -0400

    created barestbones react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let baseURL = ''
+
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3003'
+} else {
+  baseURL = 'your heroku backend url here'
 }
 
-export default App;
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      artists : []
+    }
+  }
+
+  getArtists() {
+    //Request to Spotify will go here to populate state.artists
+  }
+
+  render() {
+    return(
+      <h1>Not Spotify</h1>
+    )
+  }
+}
+
+export default App
