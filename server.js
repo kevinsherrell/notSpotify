2a8dd7ed93571f3622783bd9ad21b28a0f2b67e7
Kole Deighan <kdeigs@Koles-MacBook-Air.local>
2021-03-16 13:18:57 -0500

    Added getToken Route

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const axios = require('axios'); //For Fetch Requests from the Spotify API
 const port = 3000 // this will be moved to environment variable
 const morgan = require('morgan');
 const cors = require('cors');
+
+//Vars
+const redirect_uri = 'http://localhost:3000/loginCallback' //Need an if else statement for heroku
+const my_client_id = '39c7c593c7fc4e51873b1705c7c9a6b7' //This and secret need to be in env
+const my_client_secret = '84a861c066ec4ae4b9e6ee7b4ec3d960'
+let OAuthToken = '';
+let mainOauthCode = '';
+let refreshToken = '';
+
 // middleware
 app.use(morgan('dev')); // Morgan is for server logging
 
@@ -22,5 +31,33 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions));
+
+app.post('/giveCode', (req, res) => {
+    mainOauthCode = req.body.code;
+    const data = my_client_id + ':' + my_client_secret //https://stackabuse.com/encoding-and-decoding-base64-strings-in-node-js/
+    let buff = new Buffer(data);
+    let base64data = buff.toString('base64');
+
+    const options = {
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        headers: {
+            'Authorization': 'Basic ' + base64data,
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        params: {
+            grant_type: 'authorization_code',
+            code: mainOauthCode,
+            redirect_uri: redirect_uri,
+        }
+    } //End of Options
+    axios(options)
+    .then(res => {
+        OAuthToken = res.data.access_token;
+        refreshToken = res.data.refres_token;
+    })
+    .then(() => console.log(OAuthToken))
+    .catch(err => console.log(err));
+}) // End of app.post
 
 app.listen(port, ()=>console.log(`server is listening on port ${port}`));
