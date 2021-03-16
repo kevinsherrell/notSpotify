ce56ef6e45b12c4279cb60c3058604e996f1d35d
Kole Deighan <kdeigs@Koles-MacBook-Air.local>
2021-03-16 13:37:25 -0500

    Added loginCallback Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Router, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
+import LoginCallback from './components/LoginCallback';
 
 class App extends Component {
   render() {
@@ -9,6 +11,9 @@ class App extends Component {
         <Switch>
           <Route path='/login'>
             <Login />
+          </Route>
+          <Route path='/loginCallback'>
+            <LoginCallback />
           </Route>
         </Switch>
       </Router>
---
+++ client/src/components/LoginCallback.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import qs from 'qs';
+
+class LoginCallback extends Component {
+    componentDidMount() {
+        const OauthCode = qs.parse(this.props.location.search, { ignoreQueryPrefix: true }).code
+        
+        fetch('http://localhost:3003/giveCode', {
+            method: 'POST',
+            body: JSON.stringify({ code: OauthCode}),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+            .then(res => res.json())
+            .then(resJson => {
+                console.log(resJson)
+                window.close();
+            })
+            .catch(err => console.log({'Error': err}));
+    }
+    render() {
+        return(
+            <div>Redirecting you to the Application!</div>
+        )
+    }
+}
